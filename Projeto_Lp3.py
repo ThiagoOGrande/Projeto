9a67a434c04837283eef2100e5d5cf491187af30
ThiagoOGrande <202419810033@ifba.edu.br>
2025-11-04 10:26:12 -0300

    Create Projeto_Lp3.py

---
+++ Projeto_Lp3.py
@@ -0,0 +1,35 @@
+def mostrar_menu():
+    print("Digite o número respectivo ao que você deseja fazer a seguir: ")
+    print('''
+          1 - Cadastrar nova reserva:
+          2 - Carregar resevas existentes
+          3 - Cancelar reserva
+          4 - Sair''')
+    print()
+    
+def cadastrar_reserva():
+    print()
+def carregar_reservas():
+    print()
+def cancelar_reserva():
+    print()
+def sair():   
+    print() 
+
+def main():
+    opçao = int(input(': '))
+    if opçao == 1:
+        cadastrar_reserva()
+    elif opçao == 2:
+        carregar_reservas   
+    elif opçao == 3:
+        cancelar_reserva 
+    elif opçao == 4:
+        sair()  
+
+mostrar_menu()
+main()        
+    
+
+        
+ 
