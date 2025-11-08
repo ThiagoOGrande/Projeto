fbce872fa3da6a97c711c2063db3448827a6684b
ThiagoOGrande <202419810033@ifba.edu.br>
2025-11-08 15:27:30 -0300

    Update Projeto_Lp3.py

--- Projeto_Lp3.py
+++ Projeto_Lp3.py
@@ -4,17 +4,42 @@ def mostrar_menu():
           1 - Cadastrar nova reserva:
           2 - Carregar resevas existentes
           3 - Cancelar reserva
-          4 - Sair''')
+          4 - Calcular estatísticas
+          5 - Sair''')
     print()
+ 
     
+nome = 1
+checkin = 1
+checkout = 1
+tipo_quarto = 1
+quantidade_quartos = 1
+
+reservas = [
+    {
+        "Responsável: ": nome,
+        "Check-in: ": checkin,
+        "Check-out: ": checkout,
+        "tipo_quarto: ": tipo_quarto,
+        "quantidade_quartos": 5
+    },
+]
+
 def cadastrar_reserva():
     print()
 def carregar_reservas():
     print()
 def cancelar_reserva():
     print()
+def calcular_estatisticas():
+    print()    
 def sair():   
     print() 
+
+while 
+
+
+
 
 def main():
     opçao = int(input(': '))
