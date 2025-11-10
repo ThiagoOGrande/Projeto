9284f522dadf79ce7a43a4b99cc232c2de8c6ebd
ThiagoOGrande <202419810033@ifba.edu.br>
2025-11-10 11:53:14 -0300

    Update Projeto_Lp3.py

--- Projeto_Lp3.py
+++ Projeto_Lp3.py
@@ -1,60 +1,75 @@
+import os
+import sys
+import time
+from datetime import datetime 
+
+
 def mostrar_menu():
     print("Digite o número respectivo ao que você deseja fazer a seguir: ")
     print('''
           1 - Cadastrar nova reserva:
-          2 - Carregar resevas existentes
+          2 - Listar resevas existentes
           3 - Cancelar reserva
           4 - Calcular estatísticas
           5 - Sair''')
     print()
  
-    
-nome = 1
-checkin = 1
-checkout = 1
-tipo_quarto = 1
-quantidade_quartos = 1
+'''
+nome = input("Digite o seu nome: ")
+checkin = print("1")
+checkout = print("1")
+tipo_quarto = print("1")
+quantidade_quartos = print("1")
 
 reservas = [
-    {
-        "Responsável: ": nome,
-        "Check-in: ": checkin,
-        "Check-out: ": checkout,
-        "tipo_quarto: ": tipo_quarto,
-        "quantidade_quartos": 5
-    },
-]
+        {
+            "Responsável: ": nome,
+            "Check-in: ": checkin,
+            "Check-out: ": checkout,
+            "Tipo de quarto: ": tipo_quarto,
+            "Quantidade de quartos": 5
+        },
+    ]
+'''
 
 def cadastrar_reserva():
     print()
-def carregar_reservas():
+    print("Cadastre uma nova reserva: ")
+def listar_reservas():
     print()
+    print("Listar as reservas já existentes: ")
 def cancelar_reserva():
     print()
+    print("Cancelar reservas: ")
 def calcular_estatisticas():
-    print()    
+    print()
+    print("Calcular estatísticas: ")    
 def sair():   
-    print() 
-
-while 
-
-
+    print()
+    print("Valeu pela preferência vei, vole sempre!")  
 
 
 def main():
-    opçao = int(input(': '))
-    if opçao == 1:
-        cadastrar_reserva()
-    elif opçao == 2:
-        carregar_reservas   
-    elif opçao == 3:
-        cancelar_reserva 
-    elif opçao == 4:
-        sair()  
-
-mostrar_menu()
-main()        
+    while True:
+        os.system("clear")
+        mostrar_menu()
+        opçao = int(input(": "))
+        if opçao == 1:
+            cadastrar_reserva()
+        elif opçao == 2:
+            listar_reservas()
+        elif opçao == 3:
+            cancelar_reserva()
+        elif opçao == 4:
+            calcular_estatisticas()            
+        elif opçao == 5:
+            sair()
+            break
+        else:
+            print("Essa opçao não existe! Tente novamente.")
+            time.sleep(2)
+            continue
+        time.sleep(3)
     
-
         
- 
+main()  
