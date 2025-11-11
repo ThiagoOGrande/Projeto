9e43e687209d2619a8110a8f135b83ff4d69e59b
ThiagoOGrande <202419810033@ifba.edu.br>
2025-11-11 10:21:00 -0300

    Update Projeto_Lp3.py

--- Projeto_Lp3.py
+++ Projeto_Lp3.py
@@ -1,3 +1,4 @@
+
 import os
 import sys
 import time
@@ -8,10 +9,12 @@ def mostrar_menu():
     print("Digite o número respectivo ao que você deseja fazer a seguir: ")
     print('''
           1 - Cadastrar nova reserva:
-          2 - Listar resevas existentes
-          3 - Cancelar reserva
-          4 - Calcular estatísticas
-          5 - Sair''')
+          2 - Carregar reservas já existentes
+          3 - Consultar disponibilidade
+          4 - Cancelar reserva
+          5- Listar reservas
+          6- Exibir estatísticas gerais
+          7- Sair''')
     print()
  
 '''
@@ -31,19 +34,63 @@ reservas = [
         },
     ]
 '''
+def validar_datas():
+    while True:
+       tempo_dias = (checkout - checkin).days
+       quant_dias = tempo_dias
+       if quant_dias > maior_tempo:
+          maior_tempo = quant_dias
+          nome_maior_tempo = nome
+       if quant_dias < 0:
+          print("Data de check-in maior que de check-out")
+          time.sleep(3)
+          continue
+       os.system("clear")
+       if checkout < checkin:
+          print("O Check-out não pode ser maior que o Check-in! Faça novamente")
+          time.sleep(3)
+          continue
+
 
 def cadastrar_reserva():
+    while True: 
+       print()
+       print("Cadastre uma nova reserva: ")
+       nome = input("Digite o nome do responsável pela reserva: ").strip() 
+       if nome == "":
+          print("Nome inválido!")
+          time.sleep(3)
+          continue
+       time.sleep(2)
+    
+       checkin = int(input("Digite a data de Check-In, no formato (dd/mm/aa): "))
+       checkin = datetime.strptime(check_in, formato).date()
+       checkout = int(input("Digite a data de Check-Outn, no formato (dd/mm/aa): "))
+       checkout = datetime.strptime(check_out, formato).date()
+       os.system('clear')
+       validar_datas()
+       quarto = input("Digite o tipo de quarto que deseja:")
+
+
+
+
+def carregar_reservas():
     print()
-    print("Cadastre uma nova reserva: ")
-def listar_reservas():
+    print("Carregar reservas já existentes: ")
+def consultar_reservas():
     print()
-    print("Listar as reservas já existentes: ")
+    print("Consultar disponibilidade: ")
+
+
 def cancelar_reserva():
     print()
     print("Cancelar reservas: ")
-def calcular_estatisticas():
+def exibir_estatisticas():
     print()
     print("Calcular estatísticas: ")    
+def listar_reservas():
+    print()
+    print("Listar as reservas já existentes: ")    
 def sair():   
     print()
     print("Valeu pela preferência vei, vole sempre!")  
@@ -57,12 +104,16 @@ def main():
         if opçao == 1:
             cadastrar_reserva()
         elif opçao == 2:
-            listar_reservas()
+            carregar_reservas()  
         elif opçao == 3:
-            cancelar_reserva()
+            consultar_reservas()     
         elif opçao == 4:
-            calcular_estatisticas()            
+            cancelar_reserva()     
         elif opçao == 5:
+            listar_reservas()
+        elif opçao == 6:
+            exibir_estatisticas()            
+        elif opçao == 7:
             sair()
             break
         else:
